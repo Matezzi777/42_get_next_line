96e3a5e32845b3ebd017dc0318a5b30817c65a22
Matezzi777 <matezzi777@gmail.com>
2024-04-28 20:06:22 +0200

    .c started

--- get_next_line.c
+++ get_next_line.c
@@ -10,21 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+// #include "get_next_line.h"
+#include <stdio.h>
 
 char	*get_next_line(int fd);
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
-	int		check;
-	int		size;
-	size = SIZEBUF;
-	check = read(fd, buffer, size);
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <=0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	buffer = read_file(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = ft_line(buffer);
+	buffer = ft_next(buffer);
+	return (line);
 }
 
-/*
-
-
-
-*/
+int	main(void)
+{
+	// printf("Test :\n");
+	// if (BUFFER_SIZE > 0)
+	// 	printf("BUFFER_SIZE = %d", BUFFER_SIZE);
+	// else
+	// 	printf("BUFFER_SIZE = %d\nINVALID BUFFER SIZE", BUFFER_SIZE);
+	return (0);
+}
