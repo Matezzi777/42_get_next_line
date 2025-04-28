b5fabaacd866beede2a3497c994beec2b2efe47d
Matezzi777 <matezzi777@gmail.com>
2025-04-28 22:03:21 +0200

    Project almost ready (no bonus)

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 16:26:07 by maxmart2          #+#    #+#             */
-/*   Updated: 2025/04/25 18:55:24 by maxmart2         ###   ########.fr       */
+/*   Updated: 2025/04/28 21:44:12 by maxmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,13 @@
 # include <unistd.h> //read()
 # include <stdlib.h> //malloc()
 
+//get_next_line.c
 char	*get_next_line(int fd);
+//get_next_line_utils.c
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strchr(const char *string, int searchedChar);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+size_t	ft_strlen(const char *theString);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,84 @@
 /*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 16:25:57 by maxmart2          #+#    #+#             */
-/*   Updated: 2025/04/25 18:29:57 by maxmart2         ###   ########.fr       */
+/*   Updated: 2025/04/28 21:53:59 by maxmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	int		sizetotal;
+	int		i;
+	int		j;
+	char	*res;
+
+	i = 0;
+	sizetotal = ft_strlen(s1) + ft_strlen(s2);
+	res = (char *)malloc((sizetotal + 1) * sizeof(char));
+	if (!res || !s1 || !s2)
+		return (NULL);
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[sizetotal] = 0;
+	return (res);
+}
+
+char	*ft_strchr(const char *string, int searchedChar)
+{
+	char	*str;
+
+	str = (char *)string;
+	while (*str != searchedChar && *str != 0)
+		str++;
+	if (*str == searchedChar)
+		return (str);
+	else
+		return (NULL);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t elementCount, size_t elementSize)
+{
+	char	*res;
+
+	res = (void *)malloc(elementSize * elementCount);
+	if (!res)
+		return (NULL);
+	ft_bzero(res, elementSize * elementCount);
+	return (res);
+}
+
+size_t	ft_strlen(const char *theString)
+{
+	int	i;
+
+	i = 0;
+	while (theString[i])
+		i++;
+	return (i);
+}
